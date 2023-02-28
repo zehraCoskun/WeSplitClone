57e34d125500e1f5d69ee34902d82f4ba3892a11
Zehra Coskun <rzg>
2023-02-28 14:23:26 +0300

    güncellendi

--- WeSplitClone/ContentView.swift
+++ WeSplitClone/ContentView.swift
@@ -1,26 +1,81 @@
 //
 //  ContentView.swift
-//  WeSplitClone
+//  WeSplit
 //
-//  Created by Zehra Coşkun on 28.02.2023.
+//  Created by Zehra Coşkun on 27.02.2023.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountFocus : Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerson : Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let totalAmount = checkAmount * (100 + tipSelection)
+        let amountPerPerson = totalAmount / 100 / peopleCount
+        return amountPerPerson }
+    var totalMoney : Double {
+        let tipSelection = Double(tipPercentage)
+        return checkAmount * (100 + tipSelection) / 100
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            Form {
+                Section {
+                    TextField("Bakiye hesap", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "TRY"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountFocus)
+                    Picker("Toplam kişi", selection: $numberOfPeople){
+                        ForEach(2..<10){
+                            Text("\($0) Kişi")
+                        }
+                    }
+                }
+                Section {
+                    Picker("Bahşiş yüzdesi", selection: $tipPercentage ){
+//                        ForEach(tipPercentages, id: \.self) {
+//                            Text($0, format: .percent)
+//                        }
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                } header : {
+                    Text("Ne kadar bahşiş bırakacaksınız?")
+                }
+                Section {
+                    Text(totalMoney, format: .currency(code: Locale.current.currency?.identifier ?? "TRY"))
+                } header : {
+                    Text("Toplam tutar")
+                }
+                Section {
+                    Text(totalPerson, format: .currency(code: Locale.current.currency?.identifier ?? "TRY"))
+                } header : {
+                    Text("Kişi başına düşen tutar")
+                }
+            }
+            .navigationTitle("We Split")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Tamam") {amountFocus = false}
+                }
+            }
         }
-        .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
