c8be6c21c5e21efe0ae1a60b58333b04fd4da098
Zehra Coskun <rzg>
2023-02-28 14:20:23 +0300

    Initial Commit

---
+++ WeSplitClone/WeSplitCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitCloneApp.swift
+//  WeSplitClone
+//
+//  Created by Zehra Coşkun on 28.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
